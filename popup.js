e6036c0e7a80108ba79d92e438c06890f0df162a
Jon Fleming <jon.fleming@hotmail.com>
2022-03-01 15:31:40 -0800

    first commit

---
+++ popup.js
@@ -0,0 +1,42 @@
+const saveButton = document.getElementById("save-button")
+const LoadButton = document.getElementById("load-button")
+
+chrome.storage.sync.get("tabgroups", ({ tabgroups }) => {
+  changeColor.style.backgroundColor = color;
+})
+
+saveButton.addEventListener('click', async () => {
+  // get tab groups
+  let tabGroups = {}
+
+  // for each tab group
+  for (let tabGroup in tabgroups) {
+    // for each tab in tab group
+    for (let tab of tabgroups[tabGroup]) {
+      // add tab
+      
+    }
+  }
+  
+  // save tab groups
+  chrome.storage.sync.set({ tabGroups });
+});
+  
+  
+loadButton.addEventListener('click', async () => {
+  // load tab groups
+  chrome.storage.sync.get(['tabGroups'], ({ tabgroups }) => {
+    // for each tab group
+    for (let tabGroup in tabgroups) {
+      // for each tab in tab group
+      for (let tab of tabgroups[tabGroup]) {
+        // add tab
+        
+      }
+    }
+  })
+
+  // close existing tabs
+
+  // open tabs
+});
