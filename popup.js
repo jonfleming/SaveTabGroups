ae41111c93f92382226f48360c7b70e50b9a4c62
MSI-AEGIS\jonfl <jonfleming@hotmail.com>
2023-06-21 12:55:30 -0700

    active window

--- popup.js
+++ popup.js
@@ -1,206 +1,270 @@
-const saveButton = document.getElementById('save-button')
-const saveLink = document.getElementById('saveLink')
-const input = document.getElementById('input')
-const loadButton = document.getElementById('load-button')
-const table = document.getElementById('table')
-const message = document.getElementById('message')
-const preview = document.getElementById('preview')
-const previewHeading = document.getElementById('preview-heading')
-const groupName = document.getElementById('groupName')
+// &#9654; = ▶
+//
+const title = document.getElementById("title");
+const saveButton = document.getElementById("save-button");
+const saveLink = document.getElementById("save-link");
+const input = document.getElementById("input");
+const loadButton = document.getElementById("load-button");
+const table = document.getElementById("saved-groups-table");
+const message = document.getElementById("message");
+const preview = document.getElementById("preview");
+const previewHeading = document.getElementById("preview-heading");
+const groupName = document.getElementById("group-name");
+const noSaves = document.getElementById("no-saves");
 // yellow is 'orange' in Edge. teal is cyan, gray is grey
-const order = ['blue', 'red', 'yellow', 'green', 'pink', 'purple', 'cyan', 'grey']
-
-let tabGroupSaves = {}
-let selected = ''
+const order = [
+  "blue",
+  "red",
+  "yellow",
+  "green",
+  "pink",
+  "purple",
+  "cyan",
+  "grey",
+];
+let currentCell = table.rows[0]?.cells[0];
+let tabGroupSaves = {};
+let selected = "";
 
 function sortOrder(a, b) {
-  const ia = order.indexOf(a.color)
-  const ib = order.indexOf(b.color)
-  return ia - ib
+  const ia = order.indexOf(a.color);
+  const ib = order.indexOf(b.color);
+  return ia - ib;
 }
 
 function addRow(name) {
-  const tr = table.insertRow(-1)
-  const td = tr.insertCell(0)
-  const icon = tr.insertCell(1)
-  const text = document.createTextNode(name)
-  td.classList.add('saved-cell')
-  td.appendChild(text)
+  const tr = table.insertRow(-1);
+  const td = tr.insertCell(0);
+  const text = document.createTextNode(name);
+  td.classList.add("saved-cell");
+  td.appendChild(text);
+  noSaves.classList.add("hide");
 }
 
 function deleteRow() {
-  const tr = currentCell.parentNode
-  const name = currentCell.innerHTML
+  const tr = currentCell.parentNode;
+  const name = currentCell.innerHTML;
 
   if (confirm(`Delete saved groups [${name}]`)) {
-    table.deleteRow(tr.rowIndex)
-    delete tabGroupSaves[name]
-    chrome.storage.sync.set({ tabGroupSaves })
-    selected = '' 
-    message.innerText = `Deleted saved groups [${name}]`
-		message.style.color = 'gray'
-		clearSelection()
+    table.deleteRow(tr.rowIndex);
+    delete tabGroupSaves[name];
+    setTabGroupSaves(() => {});
+    selected = "";
+    message.innerText = `Deleted saved groups [${name}]`;
+    message.style.color = "gray";
+    clearSelection();
   }
 }
 
 function clearSelection() {
-	currentCell.classList.remove('selected')
-	currentCell = undefined
-	document.removeEventListener('onkeydown', checkKey)
-	previewHeading.classList.add('hide')
-	loadButton.classList.add('hide')
+  currentCell.classList.remove("selected");
+  currentCell = undefined;
+  document.removeEventListener("onkeydown", checkKey);
+  previewHeading.classList.add("hide");
+  loadButton.classList.add("hide");
 }
 
-function showPreview(name) {  
-  const tabGroups = tabGroupSaves[name]
-  previewHeading.classList.remove('hide')
-  let list = ''
+function showPreview(name) {
+  const tabGroups = tabGroupSaves[name];
+  previewHeading.classList.remove("hide");
+  let list = "";
   for (let i = 0; i < tabGroups?.length; i++) {
-    list += `<li>&#9654; ${tabGroups[i].title}</li>`
+    list += `<li><span class="rotate">◢</span> ${tabGroups[i].title}</li>`;
   }
 
-  preview.innerHTML = `<ul>${list}</ul>`
+  preview.innerHTML = `<ul>${list}</ul>`;
 }
 
-table.addEventListener('click', (event) => {
-	const td = event.target
-	selectCell(td)
-})
-
-saveLink.addEventListener('click', (event) => {
-	input.classList.remove('hide')
-	groupName.focus()
-})
-
-chrome.storage.sync.get(['tabGroupSaves'], (result) => {
-  if (result) {
-    console.log('storage:', result)
-    
-    if (result.tabGroupSaves) {
-      tabGroupSaves = result.tabGroupSaves
-
-      Object.keys(tabGroupSaves).forEach(save => {
-        addRow(save)      
-      })  
-    }
+input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    saveButton.click();
   }
-})
+});
+table.addEventListener("click", (event) => {
+  const td = event.target;
+  selectCell(td);
+});
 
-saveButton.addEventListener('click', async () => {
-  const groupList = []
-	const name = groupName.value
-	
+saveLink.addEventListener("click", (event) => {
+  input.classList.remove("hide");
+  groupName.focus();
+});
+
+async function saveClick() {
+  const groupList = [];
+  const name = groupName.value;
+  const currentWindow = await chrome.windows.getCurrent();
+
   if (!name) {
-    message.innerText = 'Please enter a name'
-    message.style.color = 'red'
-    return
+    message.innerText = "Please enter a name";
+    message.style.color = "red";
+    return null;
   }
-  
-  message.innerText = ''
-  const tabGroups = await chrome.tabGroups.query({})
-  tabGroups.sort(sortOrder)
-  const tabs = await chrome.tabs.query({ })
-  for (let i = 0; i < tabGroups.length; i++) {
-    const group = tabGroups[i]
-    const tabList = []
-    for (let j = 0; j < tabs.length; j++) {          
-      const tab = tabs[j]
+
+  message.innerText = "";
+  const tabGroups = await chrome.tabGroups.query({ windowId: currentWindow.id });
+  tabGroups.sort(sortOrder);
+  const tabs = await chrome.tabs.query({});
+  for (const element of tabGroups) {
+    const group = element;
+    const tabList = [];
+    for (const tab of tabs) {
       if (tab.groupId === group.id) {
-        tabList.push({ favIconUrl: tab.favIconUrl, title: tab.title, url: tab.url })
+        tabList.push({
+          favIconUrl: tab.favIconUrl,
+          title: tab.title,
+          url: tab.url,
+        });
       }
     }
-    group.tabs = tabList
-    groupList.push(group)
-  }
-    
-  tabGroupSaves[name] = groupList
-  chrome.storage.sync.set({ tabGroupSaves }, () => {
-    addRow(name)
-    message.innerHTML = `<br\><br\>Tab Groups [${name}] Saved.`
-    message.style.color = 'green'
-	});
-	
-	input.classList.add('hide')
-});
-  
-loadButton.addEventListener('click', async () => {
-  if (!selected || selected === 'Delete') {
-    message.innerText = 'Please select a saved Tab Groups'
-    message.style.color = 'red'
-    return
+    group.tabs = tabList;
+    groupList.push(group);
   }
 
-  const tabGroups = tabGroupSaves[selected]
-  tabGroups.sort(sortOrder)
+  tabGroupSaves[name] = groupList;
 
-  for (let i = 0; i < tabGroups.length; i++) {
-    const tabGroup = tabGroups[i]
-    let tabIds = []
+  // clear and rebuild Saved Groups ist
+  table.innerHTML = "<tr><th>Saved Groups</th></tr>";
+  setTabGroupSaves((item) => {
+    addRow(item);
+    message.innerHTML = `<br/><br/>Tab Groups [${name}] Saved.`;
+    message.style.color = "green";
+  });
+}
 
-    for (let j = 0; j < tabGroup.tabs.length; j++) {
-      const tab = tabGroup.tabs[j]
+async function loadClick() {
+  if (!selected || selected === "Delete") {
+    message.innerText = "Please select a saved Tab Groups";
+    message.style.color = "red";
+    return;
+  }
+
+  const tabGroups = tabGroupSaves[selected];
+  tabGroups.sort(sortOrder);
+
+  for (const element of tabGroups) {
+    const tabGroup = element;
+    let tabIds = [];
+
+    for (const element of tabGroup.tabs) {
+      const tab = element;
       const newTab = await chrome.tabs.create({
         url: tab.url,
         active: false,
-      })
+      });
 
-      if(newTab.id !== chrome.tabs.TAB_ID_NONE) {
-        tabIds.push(newTab.id)
+      if (newTab.id !== chrome.tabs.TAB_ID_NONE) {
+        tabIds.push(newTab.id);
       }
     }
 
-    const groupId = await chrome.tabs.group({tabIds})
-    chrome.tabGroups.update(groupId, { title: tabGroup.title, color: tabGroup.color, collapsed: tabGroup.collapsed })
-    
-    message.innerText = `Loaded Tab Groups ${selected}]`
-    message.style.color = 'green'
+    const groupId = await chrome.tabs.group({ tabIds });
+    chrome.tabGroups.update(groupId, {
+      title: tabGroup.title,
+      color: tabGroup.color,
+      collapsed: tabGroup.collapsed,
+    });
+
+    message.innerText = `Loaded Tab Groups ${selected}]`;
+    message.style.color = "green";
   }
-})
+}
+
+saveButton.addEventListener("click", async () => {
+  await saveClick().catch(error => console.log(error));
+  input.classList.add("hide");
+});
+
+loadButton.addEventListener("click", () => {
+  loadClick().catch(error => console.log(error));
+});
 
 // Table Navigation
 function selectCell(td) {
-	if (td != null) {
-		td.focus()
-		currentCell?.classList.remove('selected')
-		selected = td.innerHTML
-		currentCell = td;
-		td.classList.add('selected')
-		loadButton.classList.remove('hide')
-		showPreview(selected)
-		document.onkeydown = checkKey
+  if (td?.tagName === "TD") {
+    td.focus();
+    currentCell?.classList.remove("selected");
+    selected = td.innerHTML;
+    currentCell = td;
+    td.classList.add("selected");
+    loadButton.classList.remove("hide");
+    showPreview(selected);
+    document.onkeydown = checkKey;
+  } else {
+    loadButton.classList.add("hide");
+    previewHeading.classList.add("hide");
+    preview.innerHTML = "";
   }
 }
 
 function checkKey(e) {
-  e = e || window.event;
-	if (e.keyCode == '38') {
-		// up arrow
-		var idx = currentCell.cellIndex;
-		var nextrow = currentCell.parentElement.previousElementSibling;
-		if (nextrow != null) {
-			var sibling = nextrow.cells[idx];
-			selectCell(sibling);
-		}
-	} else if (e.keyCode == '40') {
-		// down arrow
-		var idx = currentCell.cellIndex;
-		var nextrow = currentCell.parentElement.nextElementSibling;
-		if (nextrow != null) {
-			var sibling = nextrow.cells[idx];
-			selectCell(sibling);
-		}
-	} else if (e.keyCode == '37') {
-		// left arrow
-		var sibling = currentCell.previousElementSibling;
-		selectCell(sibling);
-	} else if (e.keyCode == '39') {
-		// right arrow
-		var sibling = currentCell.nextElementSibling;
-		selectCell(sibling);
-	} else if (e.keyCode == '46') {
-		// delete
-		deleteRow(currentCell)
-	}
+  let idx, nextrow, sibling;
+  if (e.keyCode == "38") {
+    // up arrow
+    idx = currentCell.cellIndex;
+    nextrow = currentCell.parentElement.previousElementSibling;
+    if (nextrow != null) {
+      sibling = nextrow.cells[idx];
+      selectCell(sibling);
+    }
+  } else if (e.keyCode == "40") {
+    // down arrow
+    idx = currentCell.cellIndex;
+    nextrow = currentCell.parentElement.nextElementSibling;
+    if (nextrow != null) {
+      sibling = nextrow.cells[idx];
+      selectCell(sibling);
+    }
+  } else if (e.keyCode == "37") {
+    // left arrow
+    sibling = currentCell.previousElementSibling;
+    selectCell(sibling);
+  } else if (e.keyCode == "39") {
+    // right arrow
+    sibling = currentCell.nextElementSibling;
+    selectCell(sibling);
+  } else if (e.keyCode == "46") {
+    // delete
+    deleteRow();
+  }
 }
 
-var currentCell = table.rows[0]?.cells[0]
+function getTabGroupSaves(action) {
+  tabGroupSaves = {};
+
+  chrome.storage.local.get(["saves"]).then((response) => {
+    if (response) {
+      console.log("storage:", response);
+      const groups = response.saves;
+      for (let save of groups) {
+        chrome.storage.local.get([save], (tabs) => {
+          console.log("storage:", tabs);
+          tabGroupSaves[save] = tabs[save];
+          action(save);
+        });
+      }
+    }
+  });
+}
+
+function setTabGroupSaves(action) {
+  const saves = Object.keys(tabGroupSaves);
+
+  chrome.storage.local.set({ saves }).then(() => {
+    for (let save of saves) {
+      const storageItem = {};
+      storageItem[save] = tabGroupSaves[save];
+      chrome.storage.local.set(storageItem).then(() => {
+        console.log("saved ", save);
+        action(save);
+      });
+    }
+  });
+}
+
+title.addEventListener("click", () => {
+  // debugging
+  // getTabGroupSaves((name) => addRow(name))
+});
+
+getTabGroupSaves((name) => addRow(name));
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 Chrome Extension to save currently opened tab groups to be reloaded later.
 
 Developed by Jon Fleming
-https://github.com/jonfleming/SaveTabGroups
+[GitHub](https://github.com/jonfleming/SaveTabGroups)
